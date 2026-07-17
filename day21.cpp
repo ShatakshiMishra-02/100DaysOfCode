cf96f0d48b43ff89973134c3e1187f6ddcbc8dc0
ShatakshiMishra-02 <shatakshi2005mishra@gmail.com>
2026-07-17 23:19:43 +0530

    day21 code

---
+++ day21.cpp
@@ -0,0 +1,63 @@
+// Problem: Create and Traverse Singly Linked List
+
+// Input:
+// - First line: integer n
+// - Second line: n space-separated integers
+
+// Output:
+// - Print the result
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+
+#include <iostream>
+using namespace std;
+
+// Node structure
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+
+        Node* newNode = new Node(x);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Traverse and print linked list
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
