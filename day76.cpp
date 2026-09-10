23ae9665bffcf7a24ece12b98c9ae10cf948c563
Shatakshi <shatakshi2005mishra@gmail.com>
2026-09-10 19:38:52 +0530

    day76 code

---
+++ day76.cpp
@@ -0,0 +1,68 @@
+// Problem Statement
+// Using DFS or BFS, count number of connected components.
+
+// Input Format
+// n m
+// edges
+
+// Output Format
+// Number of connected components.
+
+// Sample Input
+// 6 3
+// 1 2
+// 2 3
+// 5 6
+
+// Sample Output
+// 3
+
+// Explanation
+// Components: {1,2,3}, {4}, {5,6}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n + 1);
+
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<bool> visited(n + 1, false);
+    int components = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            components++;
+
+            queue<int> q;
+            q.push(i);
+            visited[i] = true;
+
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+
+                for (int neighbor : adj[node]) {
+                    if (!visited[neighbor]) {
+                        visited[neighbor] = true;
+                        q.push(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << components << endl;
+
+    return 0;
+}
